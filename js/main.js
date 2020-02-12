f6555b72364d4d260b3290ac6f5a178d405bc92f
Danis Haertdinov <danis.haertdinov@gmail.com>
2020-02-12 21:18:02 +0300

    добавляет функционал открытия и закрытия карточек обьявлений

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ var NUMBER_OF_MOCKS = 8;
 var MAP_PIN_HEIGHT = 70;
 var MAP_PIN_WIDTH = 50;
 var ENTER_KEY = 'Enter';
+var ESCAPE_KEY = 'Escape';
 var ACTIVE_MAIN_MAP_PIN_HEIGHT = 84;
 var map = document.querySelector('.map');
 var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-// var mapCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
-// var mapFiltersContainer = map.querySelector('.map__filters-container');
+var mapCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+var mapFiltersContainer = map.querySelector('.map__filters-container');
 var adFilterForm = document.querySelector('.ad-form');
 var roomsSelect = adFilterForm.querySelector('#room_number');
 var guestsSelect = adFilterForm.querySelector('#capacity');
@@ -150,11 +151,19 @@ var createMocks = function (number) {
 
 var createMapPin = function (mock) {
   var mapPin = mapPinTemplate.cloneNode(true);
-  var newPinImg = mapPin.querySelector('img');
+  var mapPinImg = mapPin.querySelector('img');
   mapPin.style.top = mock.location.y - MAP_PIN_HEIGHT + 'px';
   mapPin.style.left = mock.location.x - MAP_PIN_WIDTH / 2 + 'px';
-  newPinImg.src = mock.author.avatar;
-  newPinImg.alt = mock.offer.title;
+  mapPinImg.src = mock.author.avatar;
+  mapPinImg.alt = mock.offer.title;
+  mapPin.addEventListener('click', function () {
+    showMapCard(mock);
+  });
+  mapPin.addEventListener('keydown', function (evt) {
+    if (evt.key === ENTER_KEY) {
+      showMapCard(mock);
+    }
+  });
   return mapPin;
 };
 
@@ -172,74 +181,96 @@ var activatePage = function () {
   mainMapPin.removeEventListener('keydown', mainMapPinEnterPressHandler);
 };
 
-// var flexNormalize = function (number, forms) {
-//   number = Number(number);
-//   if (number % 100 === 11) {
-//     return forms[0];
-//   }
-//   var remainder = number % 10;
-//   switch (true) {
-//     case remainder === 0 || remainder > 4:
-//       return forms[0];
-//     case remainder === 1:
-//       return forms[1];
-//     default:
-//       return forms[2];
-//   }
-// };
+var flexNormalize = function (number, forms) {
+  number = Number(number);
+  if (number % 100 === 11) {
+    return forms[0];
+  }
+  var remainder = number % 10;
+  switch (true) {
+    case remainder === 0 || remainder > 4:
+      return forms[0];
+    case remainder === 1:
+      return forms[1];
+    default:
+      return forms[2];
+  }
+};
 
-// var roomsFlexNormalize = function (number) {
-//   var forms = ['комнат', 'комната', 'комнаты'];
-//   return flexNormalize(number, forms);
-// };
+var roomsFlexNormalize = function (number) {
+  var forms = ['комнат', 'комната', 'комнаты'];
+  return flexNormalize(number, forms);
+};
 
-// var guestsFlexNormalize = function (number) {
-//   var forms = ['гостей', 'гостя', 'гостей'];
-//   return flexNormalize(number, forms);
-// };
+var guestsFlexNormalize = function (number) {
+  var forms = ['гостей', 'гостя', 'гостей'];
+  return flexNormalize(number, forms);
+};
 
-// var offerTypeListMap = {
-//   'flat': 'Квартира',
+var offerTypeListMap = {
+  'flat': 'Квартира',
 
-//   'bungalo': 'Бунгало',
+  'bungalo': 'Бунгало',
 
-//   'house': 'Дом',
+  'house': 'Дом',
 
-//   'palace': 'Дворец',
+  'palace': 'Дворец',
 
-// };
+};
 
-// var createMapCardFeature = function (featureData) {
-//   return mapCardTemplate
-//   .querySelector('.popup__feature--' + featureData)
-//   .cloneNode(true);
-// };
+var createMapCardFeature = function (featureData) {
+  return mapCardTemplate
+  .querySelector('.popup__feature--' + featureData)
+  .cloneNode(true);
+};
 
-// var createMapCardPhoto = function (photoSource) {
-//   var photo = mapCardTemplate.querySelector('.popup__photo').cloneNode(true);
-//   photo.src = photoSource;
-//   return photo;
-// };
+var createMapCardPhoto = function (photoSource) {
+  var photo = mapCardTemplate.querySelector('.popup__photo').cloneNode(true);
+  photo.src = photoSource;
+  return photo;
+};
 
-// var showMapCard = function (offerData) {
-//   var rooms = offerData.offer.rooms;
-//   var guests = offerData.offer.guests;
-//   var mapCard = mapCardTemplate.cloneNode(true);
-//   mapCard.querySelector('.popup__title').textContent = offerData.offer.title;
-//   mapCard.querySelector('.popup__text--address').textContent = offerData.offer.address;
-//   mapCard.querySelector('.popup__text--price').textContent = offerData.offer.price + '₽/ночь';
-//   mapCard.querySelector('.popup__type').textContent = offerTypeListMap[offerData.offer.type];
-//   mapCard.querySelector('.popup__text--capacity').textContent = rooms + ' ' + roomsFlexNormalize(rooms) + ' для ' + guests + ' ' + guestsFlexNormalize(guests);
-//   mapCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + offerData.offer.checkIn + ', выезд до ' + offerData.offer.checkOut;
-//   mapCard.querySelector('.popup__features').innerHTML = '';
-//   mapCard.querySelector('.popup__features').appendChild(collectElements(offerData.offer.features, createMapCardFeature));
-//   mapCard.querySelector('.popup__description').textContent = offerData.offer.description;
-//   mapCard.querySelector('.popup__photos').innerHTML = '';
-//   mapCard.querySelector('.popup__photos').appendChild(collectElements(offerData.offer.photos, createMapCardPhoto));
-//   mapCard.querySelector('.popup__avatar').src = offerData.author.avatar;
+var showMapCard = function (offerData) {
+  var rooms = offerData.offer.rooms;
+  var guests = offerData.offer.guests;
+  var mapCard = mapCardTemplate.cloneNode(true);
+  mapCard.querySelector('.popup__title').textContent = offerData.offer.title;
+  mapCard.querySelector('.popup__text--address').textContent = offerData.offer.address;
+  mapCard.querySelector('.popup__text--price').textContent = offerData.offer.price + '₽/ночь';
+  mapCard.querySelector('.popup__type').textContent = offerTypeListMap[offerData.offer.type];
+  mapCard.querySelector('.popup__text--capacity').textContent = rooms + ' ' + roomsFlexNormalize(rooms) + ' для ' + guests + ' ' + guestsFlexNormalize(guests);
+  mapCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + offerData.offer.checkIn + ', выезд до ' + offerData.offer.checkOut;
+  mapCard.querySelector('.popup__features').innerHTML = '';
+  mapCard.querySelector('.popup__features').appendChild(collectElements(offerData.offer.features, createMapCardFeature));
+  mapCard.querySelector('.popup__description').textContent = offerData.offer.description;
+  mapCard.querySelector('.popup__photos').innerHTML = '';
+  mapCard.querySelector('.popup__photos').appendChild(collectElements(offerData.offer.photos, createMapCardPhoto));
+  mapCard.querySelector('.popup__avatar').src = offerData.author.avatar;
+  removeMapCard();
+  mapCard.querySelector('.popup__close').addEventListener('click', function () {
+    removeMapCard();
+  });
+  mapCard.querySelector('.popup__close').addEventListener('keydown', function (evt) {
+    if (evt.key === ENTER_KEY) {
+      removeMapCard();
+    }
+  });
+  document.addEventListener('keydown', mapCardEscPressHandler);
+  map.insertBefore(mapCard, mapFiltersContainer);
+};
 
-//   map.insertBefore(mapCard, mapFiltersContainer);
-// };
+var removeMapCard = function () {
+  if (map.querySelector('.map__card')) {
+    map.querySelector('.map__card').remove();
+    document.removeEventListener('keydown', mapCardEscPressHandler);
+  }
+};
+
+var mapCardEscPressHandler = function (evt) {
+  if (evt.key === ESCAPE_KEY) {
+    removeMapCard();
+  }
+};
 
 var disablePageFormElements = function () {
   document.querySelectorAll('fieldset,input,select').forEach(function (element) {
@@ -301,5 +332,4 @@ setAdAddress();
 mainMapPin.addEventListener('mousedown', mainMapPinLeftMouseDownHandler);
 mainMapPin.addEventListener('keydown', mainMapPinEnterPressHandler);
 var mocks = createMocks(NUMBER_OF_MOCKS);
-// activateMap();
-// showMapCard(mocks[0]);
+
