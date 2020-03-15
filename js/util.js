cd8080975f6a02e9afb6154281f75c32b40b51be
Danis Haertdinov <danis.haertdinov@gmail.com>
2020-03-15 18:58:29 +0300

    удалить не используемые функции

--- js/util.js
+++ js/util.js
@@ -5,29 +5,6 @@
   var ESCAPE_KEY = 'Escape';
   var DEBOUNCE_INTERVAL = 500;
 
-  var getRandomNumber = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-  var getRandomElementOfArray = function (array) {
-    return array[getRandomNumber(0, array.length - 1)];
-  };
-
-  var shuffleArray = function (array) {
-    var j;
-    var temp;
-    for (var i = array.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      temp = array[j];
-      array[j] = array[i];
-      array[i] = temp;
-    }
-    return array;
-  };
-
-  var getRandomLengthArray = function (array) {
-    return shuffleArray(array).slice(getRandomNumber(0, array.length - 1));
-  };
-
   var collectElements = function (data, callBackFunction) {
     var fragment = document.createDocumentFragment();
     data.forEach(function (element) {
@@ -88,10 +65,6 @@
   };
 
   window.util = {
-    getRandomNumber: getRandomNumber,
-    getRandomElementOfArray: getRandomElementOfArray,
-    shuffleArray: shuffleArray,
-    getRandomLengthArray: getRandomLengthArray,
     collectElements: collectElements,
     flexNormalize: flexNormalize,
     removeElementsByClassName: removeElementsByClassName,
